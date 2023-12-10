0b463319710c4b2f3381acbebd4c9b727e69577d
satyam46020 <satyam46020@gmail.com>
2023-12-10 22:22:13 +0530

    portfolio

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,105 @@
+import { Box, Button, Flex, Image, Text } from "@chakra-ui/react";
+import { VscCloudDownload } from "react-icons/vsc";
+import MyResume from "../assets/MyResume.pdf";
+import hero from "./ProfileImages/hero.jpg";
+import { useState } from "react";
+
+export const Home = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const handleLoad = () => {
+    setIsLoaded(true);
+  };
+  return (
+    <>
+      <Box height={{ base: "40px", md: "60px" }}></Box>
+      <Box
+        mt={100}
+        w="100%"
+        h={{
+          base: "450px",
+          md: "400px",
+          lg: "500px",
+          sm: "320px",
+        }}
+        bgPosition="cover"
+        bgRepeat="no-repeat"
+      >
+        <Flex
+          w={{ base: "100%", md: "80%" }}
+          m="auto"
+          alignItems="center"
+          justifyContent={{ base: "center", md: "space-around" }}
+          flexDirection={{ base: "column-reverse", lg: "row" }}
+          rowGap={{ base: "10px", md: "10px" }}
+          mt={{ base: "20px", md: "10px", lg: "40px" }}
+        >
+          <Box
+            fontSize={{ base: "18px", md: "35px", lg: "55px" }}
+            width={{ base: "90%", md: "100%", lg: "65%" }}
+            m={{ base: "auto", lg: "0" }}
+            fontWeight="600"
+            textAlign={{ base: "center", lg: "left" }}
+            lineHeight={{ base: "28px", md: "50px", lg: "80px" }}
+          >
+            <Flex
+              justifyContent="center"
+              gap="5px"
+              direction={{ base: "row", lg: "column" }}
+            >
+              <Flex>
+                <Text>
+                  <b>Hello</b>
+                </Text>
+                <Image
+                  mt="-5px"
+                  w={{ base: "40px", md: "60px", lg: "85px" }}
+                  src="https://cliply.co/wp-content/uploads/2021/02/392102850_EARTH_EMOJI_400px.gif"
+                />
+              </Flex>
+
+              <Text
+                fontSize={{ xs: "15px", base: "20px", md: "40px", lg: "60px" }}
+                color=""
+                style={{ color: "teal" }}
+              >
+                I am Satyam Kumar
+              </Text>
+            </Flex>
+            <Text fontSize={{ base: "15px", md: "30px", lg: "40px" }}>
+              Full Stack Web Developer
+            </Text>
+            <a href={MyResume} download>
+              <Button
+                size={{ base: "sm", md: "md", lg: "lg" }}
+                mt={{ base: "10px", md: "0" }}
+                colorScheme="teal"
+                gap={1.5}
+                borderRadius={20}
+                borderBottom="5px solid gray"
+              >
+                Resume <VscCloudDownload color="white" />
+              </Button>
+            </a>
+          </Box>
+          <Box>
+            <Image
+              src={hero}
+              shadow="2xl"
+              p="2"
+              borderBottom="6px solid teal"
+              mt={{ base: "10px", md: "0px" }}
+              w={{ base: "100px", md: "200px", lg: "350px" }}
+              borderRadius="50%"
+              _hover={{ boxshodow: "lg" }}
+              onLoad={handleLoad}
+              opacity={isLoaded ? 1 : 0}
+              transition="opacity 1s ease-in-out"
+              alt="Satyam"
+            />
+          </Box>
+        </Flex>
+      </Box>
+    </>
+  );
+};
---
+++ src/Pages/Project.jsx
@@ -0,0 +1,535 @@
+import React, { useContext } from "react";
+import { Box, Image, Badge, Text, Flex, Button } from "@chakra-ui/react";
+import { AppContext } from "../Context/Theme";
+import Jiomart from "./ProjectImages/Jiomart.png";
+import myntra from "./ProjectImages/myntra.png";
+import pharmacy from "./ProjectImages/pharmacy.png";
+import Portfolio from "./ProjectImages/Portfolio.png"
+
+export const Project = () => {
+  const { Theme } = useContext(AppContext);
+
+  const light = {
+    backgroundColor: "white",
+    color: "black",
+  };
+
+  const dark = {
+    backgroundColor: "black",
+    color: "white",
+  };
+  return (
+    <Box>
+      <Text>
+        <Text
+          textAlign="center"
+          fontSize={{ base: "20px", md: "22px", lg: "40px" }}
+          fontWeight={{ base: "600", md: "700" }}
+        >
+          My Projects
+        </Text>
+      </Text>
+
+      <Box
+        w={"80%"}
+        display={{ base: "column", sm: "column", lg: "grid" }}
+        gridTemplateColumns="repeat(2,1fr)"
+        m={"auto"}
+        mt="20px"
+        gap={20}
+      >
+        {/* First Project */}
+        <Text left>
+          <Box
+            style={Theme === "light" ? light : dark}
+            w={"100%"}
+            borderRadius="lg"
+            overflow="hidden"
+            backgroundColor={"white"}
+            mb={10}
+          >
+            <Image
+              w="full"
+              src={myntra}
+              alt="myntra"
+              h={{ lg: "300px" }}
+            />
+            <Box p="6">
+              <Text
+                fontWeight="bold"
+                fontSize={{ base: "xl", md: "2xl", lg: "3xl" }}
+              >
+                Myntra
+              </Text>
+              <Box
+                fontSize={{ base: "sm", md: "md", lg: "md" }}
+                h={{ lg: "80px" }}
+                mb={{ lg: 6 }}
+              >
+                Myntra is a stylish and user-centric e-commerce platform that brings the latest fashion trends to your fingertips. This Myntra clone website is designed to offer a seamless and enjoyable online shopping experience, allowing users to explore, discover, and shop for a wide range of fashion.
+              </Box>
+              <Box
+                display={{ base: "column", sm: "column", lg: "flex" }}
+                alignItems="baseline"
+                mt={3}
+              >
+                <Badge
+                  colorScheme="teal"
+                  borderRadius="full"
+                  px="2"
+                  fontSize={{ base: "sm", md: "sm", lg: "sm" }}
+                >
+                  TECH STACK
+                </Badge>
+                <Box
+                  display={{ base: "grid", sm: "grid", lg: "flex" }}
+                  gridTemplateColumns="repeat(1,1fr)"
+                  gap={1}
+                >
+                  <Box
+                    color="gray.500"
+                    fontWeight="semibold"
+                    letterSpacing="wide"
+                    fontSize={{ base: "xs", md: "sm", lg: "sm" }}
+                    textTransform="uppercase"
+                    ml="2"
+                  >
+                    HTML
+                  </Box>
+                  <Box
+                    color="gray.500"
+                    fontWeight="semibold"
+                    letterSpacing="wide"
+                    fontSize={{ base: "xs", md: "sm", lg: "sm" }}
+                    textTransform="uppercase"
+                    ml="2"
+                  >
+                    CSS
+                  </Box>
+                  <Box
+                    color="gray.500"
+                    fontWeight="semibold"
+                    letterSpacing="wide"
+                    fontSize={{ base: "xs", md: "sm", lg: "sm" }}
+                    textTransform="uppercase"
+                    ml="2"
+                  >
+                    Javascript
+                  </Box>
+                </Box>
+              </Box>
+            </Box>
+            <Flex
+              gap={10}
+              alignItems="center"
+              justifyContent="space-between"
+              mb={5}
+              ml={7}
+            >
+              <a
+                href="https://github.com/satyam46020/Myntra-clone"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Button
+                  size={{ base: "xs", md: "md", lg: "md" }}
+                  colorScheme="teal"
+                  borderRadius={20}
+                  borderBottom="5px solid gray"
+                >
+                  Source Code
+                </Button>
+              </a>
+              <a
+                href="https://650b2f84de1078243d876e0c--effervescent-tulumba-71b6b7.netlify.app/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Button
+                  size={{ base: "xs", md: "md", lg: "md" }}
+                  colorScheme="teal"
+                  borderRadius={20}
+                  borderBottom="5px solid gray"
+                >
+                  Deploy URL
+                </Button>
+              </a>
+            </Flex>
+          </Box>
+        </Text>
+        {/* {second} */}
+        <Text right>
+          <Box
+            style={Theme === "light" ? light : dark}
+            w={"100%"}
+            borderRadius="lg"
+            overflow="hidden"
+            backgroundColor={"white"}
+            mb={10}
+          >
+            <Image
+              w="full"
+              src={Jiomart}
+              alt="Jiomart"
+              h={{ lg: "300px" }}
+            />
+            <Box p="6">
+              <Text
+                fontWeight="bold"
+                fontSize={{ base: "xl", md: "2xl", lg: "3xl" }}
+              >
+                Jio Mart
+              </Text>
+              <Box
+                fontSize={{ base: "sm", md: "md", lg: "md" }}
+                h={{ lg: "80px" }}
+                mb={{ lg: 6 }}
+              >
+                Jio Mart is a clone of Jiomart.com. Jio Mart is India's most convenient online grocery website. No more hassles of sweating it out in crowded markets, grocery shops & supermarkets - now shop from the comfort of your home, office or on the move.
+              </Box>
+              <Box
+                display={{ base: "column", sm: "column", lg: "flex" }}
+                alignItems="baseline"
+                mt={3}
+              >
+                <Badge
+                  colorScheme="teal"
+                  borderRadius="full"
+                  px="2"
+                  fontSize={{ base: "sm", md: "sm", lg: "sm" }}
+                >
+                  TECH STACK
+                </Badge>
+                <Box
+                  display={{ base: "grid", sm: "grid", lg: "flex" }}
+                  gridTemplateColumns="repeat(1,1fr)"
+                  gap={1}
+                >
+                  
+                  <Box
+                    color="gray.500"
+                    fontWeight="semibold"
+                    letterSpacing="wide"
+                    fontSize={{ base: "xs", md: "sm", lg: "sm" }}
+                    textTransform="uppercase"
+                    ml="2"
+                  >
+                    React
+                  </Box>
+                  <Box
+                    color="gray.500"
+                    fontWeight="semibold"
+                    letterSpacing="wide"
+                    fontSize={{ base: "xs", md: "sm", lg: "sm" }}
+                    textTransform="uppercase"
+                    ml="2"
+                  >
+                    Redux
+                  </Box>
+                  <Box
+                    color="gray.500"
+                    fontWeight="semibold"
+                    letterSpacing="wide"
+                    fontSize={{ base: "xs", md: "sm", lg: "sm" }}
+                    textTransform="uppercase"
+                    ml="2"
+                  >
+                    Chakra-UI
+                  </Box>
+                  <Box
+                    color="gray.500"
+                    fontWeight="semibold"
+                    letterSpacing="wide"
+                    fontSize={{ base: "xs", md: "sm", lg: "sm" }}
+                    textTransform="uppercase"
+                    ml="2"
+                  >
+                    Javascript
+                  </Box>
+                </Box>
+              </Box>
+            </Box>
+            <Flex
+              gap={10}
+              alignItems="center"
+              justifyContent="space-between"
+              mb={5}
+              ml={7}
+            >
+              <a
+                href="https://github.com/satyam46020/jio-mart"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Button
+                  size={{ base: "xs", md: "md", lg: "md" }}
+                  colorScheme="teal"
+                  borderRadius={20}
+                  borderBottom="5px solid gray"
+                >
+                  Source Code
+                </Button>
+              </a>
+              <a
+                href="https://jiomart-com-rct-201-shashikantrupin.vercel.app/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Button
+                  size={{ base: "xs", md: "md", lg: "md" }}
+                  colorScheme="teal"
+                  borderRadius={20}
+                  borderBottom="5px solid gray"
+                >
+                  Deploy URL
+                </Button>
+              </a>
+            </Flex>
+
+          </Box>
+        </Text>
+        {/* third */}
+        <Text right>
+          <Box
+            style={Theme === "light" ? light : dark}
+            w={"100%"}
+            borderRadius="lg"
+            overflow="hidden"
+            backgroundColor={"white"}
+            mb={10}
+          >
+            <Image
+              w="full"
+              src={pharmacy}
+              alt="pharmacy"
+              h={{ lg: "300px" }}
+            />
+            <Box p="6">
+              <Text
+                fontWeight="bold"
+                fontSize={{ base: "xl", md: "2xl", lg: "3xl" }}
+              >
+                Pharmacy
+              </Text>
+              <Box
+                fontSize={{ base: "sm", md: "md", lg: "md" }}
+                h={{ lg: "80px" }}
+                mb={{ lg: 6 }}
+              >
+                Pharmacy is an innovative Pharmacy Management System designed to streamline and modernize pharmacy operations. This comprehensive software solution empowers pharmacies to efficiently manage inventory, enhance customer service, and maintain accurate records.
+              </Box>
+              <Box
+                display={{ base: "column", sm: "column", lg: "flex" }}
+                alignItems="baseline"
+                mt={3}
+              >
+                <Badge
+                  colorScheme="teal"
+                  borderRadius="full"
+                  px="2"
+                  fontSize={{ base: "sm", md: "sm", lg: "sm" }}
+                >
+                  TECH STACK
+                </Badge>
+                <Box
+                  display={{ base: "grid", sm: "grid", lg: "flex" }}
+                  gridTemplateColumns="repeat(1,1fr)"
+                  gap={1}
+                >
+                  
+                  <Box
+                    color="gray.500"
+                    fontWeight="semibold"
+                    letterSpacing="wide"
+                    fontSize={{ base: "xs", md: "sm", lg: "sm" }}
+                    textTransform="uppercase"
+                    ml="2"
+                  >
+                    HTML
+                  </Box>
+                  <Box
+                    color="gray.500"
+                    fontWeight="semibold"
+                    letterSpacing="wide"
+                    fontSize={{ base: "xs", md: "sm", lg: "sm" }}
+                    textTransform="uppercase"
+                    ml="2"
+                  >
+                    CSS
+                  </Box>
+                  <Box
+                    color="gray.500"
+                    fontWeight="semibold"
+                    letterSpacing="wide"
+                    fontSize={{ base: "xs", md: "sm", lg: "sm" }}
+                    textTransform="uppercase"
+                    ml="2"
+                  >
+                    Javascript
+                  </Box>
+                </Box>
+              </Box>
+            </Box>
+            <Flex
+              gap={10}
+              alignItems="center"
+              justifyContent="space-between"
+              mb={5}
+              ml={7}
+            >
+              <a
+                href="https://github.com/satyam46020/joyful-ring-9720"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Button
+                  size={{ base: "xs", md: "md", lg: "md" }}
+                  colorScheme="teal"
+                  borderRadius={20}
+                  borderBottom="5px solid gray"
+                >
+                  Source Code
+                </Button>
+              </a>
+              <a
+                href="https://653654a73f8e7f009d227893--bespoke-raindrop-bfcf0e.netlify.app/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Button
+                  size={{ base: "xs", md: "md", lg: "md" }}
+                  colorScheme="teal"
+                  borderRadius={20}
+                  borderBottom="5px solid gray"
+                >
+                  Deploy URL
+                </Button>
+              </a>
+            </Flex>
+
+          </Box>
+        </Text>
+        {/* fourth */}
+        <Text left>
+          <Box
+            style={Theme === "light" ? light : dark}
+            w={"100%"}
+            borderRadius="lg"
+            overflow="hidden"
+            backgroundColor={"white"}
+            mb={10}
+          >
+            <Image
+              w="full"
+              src={Portfolio}
+              alt="Portfolio"
+              h={{ lg: "300px" }}
+            />
+            <Box p="6">
+              <Text
+                fontWeight="bold"
+                fontSize={{ base: "xl", md: "2xl", lg: "3xl" }}
+              >
+                Portfolio
+              </Text>
+              <Box
+                fontSize={{ base: "sm", md: "md", lg: "md" }}
+                h={{ lg: "80px" }}
+                mb={{ lg: 6 }}
+              >
+                This is my portfolio project! This project contains all the valuable insights of me, showcasing my skills, projects, and achievements.
+              </Box>
+              <Box
+                display={{ base: "column", sm: "column", lg: "flex" }}
+                alignItems="baseline"
+                mt={3}
+              >
+                <Badge
+                  colorScheme="teal"
+                  borderRadius="full"
+                  px="2"
+                  fontSize={{ base: "sm", md: "sm", lg: "sm" }}
+                >
+                  TECH STACK
+                </Badge>
+                <Box
+                  display={{ base: "grid", sm: "grid", lg: "flex" }}
+                  gridTemplateColumns="repeat(1,1fr)"
+                  gap={1}
+                >
+                  <Box
+                    color="gray.500"
+                    fontWeight="semibold"
+                    letterSpacing="wide"
+                    fontSize={{ base: "xs", md: "sm", lg: "sm" }}
+                    textTransform="uppercase"
+                    ml="2"
+                  >
+                    React
+                  </Box>
+                  <Box
+                    color="gray.500"
+                    fontWeight="semibold"
+                    letterSpacing="wide"
+                    fontSize={{ base: "xs", md: "sm", lg: "sm" }}
+                    textTransform="uppercase"
+                    ml="2"
+                  >
+                    chakra-UI
+                  </Box>
+                  <Box
+                    color="gray.500"
+                    fontWeight="semibold"
+                    letterSpacing="wide"
+                    fontSize={{ base: "xs", md: "sm", lg: "sm" }}
+                    textTransform="uppercase"
+                    ml="2"
+                  >
+                    Javascript
+                  </Box>
+                </Box>
+              </Box>
+            </Box>
+            <Flex
+              gap={10}
+              alignItems="center"
+              justifyContent="space-between"
+              mb={5}
+              ml={7}
+            >
+              <a
+                href="https://github.com/satyam46020/satyam46020.github.io"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Button
+                  size={{ base: "xs", md: "md", lg: "md" }}
+                  colorScheme="teal"
+                  borderRadius={20}
+                  borderBottom="5px solid gray"
+                >
+                  Source Code
+                </Button>
+              </a>
+              <a
+                href="https://satyam46020.github.io/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Button
+                  size={{ base: "xs", md: "md", lg: "md" }}
+                  colorScheme="teal"
+                  borderRadius={20}
+                  borderBottom="5px solid gray"
+                >
+                  Deploy URL
+                </Button>
+              </a>
+            </Flex>
+          </Box>
+        </Text>
+      </Box>
+    </Box>
+  );
+};
+
